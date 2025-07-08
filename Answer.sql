ac3810bf30beb5b9f4b62f9dbc599ccafa2bd3eb
Kang Hansol <104417575+kkangsol@users.noreply.github.com>
2025-07-08 17:46:13 +0900

    Update Answer.sql

--- Answer.sql
+++ Answer.sql
@@ -1,13 +1,12 @@
-/*
- 1번
+
+-- 1번
 SELECT ename, job, sal, TO_CHAR(hiredate, 'yyyy/mm/dd') FROM emp WHERE TO_CHAR(hiredate, 'yyyy') = 1981 AND job = 'SALESMAN' ORDER BY sal DESC;
 
- 2번
+-- 2번
 SELECT ename, deptno, TO_CHAR(hiredate, 'yyyy/mm/dd') FROM emp WHERE TO_CHAR(hiredate, 'mm') = '12';
 
- 3번
+-- 3번
 SELECT ename, deptno, sal, comm FROM emp WHERE (comm IS NULL OR comm = 0) AND sal = 1500;
 
- 4번
+-- 4번
 SELECT ename, deptno, sal, (SELECT avg(sal) FROM emp)AS 급여평균 FROM emp WHERE deptno = 20 AND sal > (SELECT avg(sal) FROM emp);
- */
